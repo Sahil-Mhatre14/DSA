ffcff5d5957ae74ce5c5fb025b29689732c44e98
Sahil-Mhatre14 <sahil81623@gmail.com>
2024-02-29 23:38:34 +0530

    implement hash table

---
+++ HashTable.js
@@ -0,0 +1,64 @@
+class HashTable {
+    constructor(size){
+      this.data = new Array(size);
+      // this.data = [];
+    }
+  
+    _hash(key) {
+      let hash = 0;
+      for (let i =0; i < key.length; i++){
+          hash = (hash + key.charCodeAt(i) * i) % this.data.length
+      }
+      return hash;
+    }
+  
+    set(key, value) {
+      let address = this._hash(key);
+      if (!this.data[address]) {
+        this.data[address] = [];
+      }
+      this.data[address].push([key, value]);
+      return this.data;
+    }
+  
+    get(key){
+      const address = this._hash(key);
+      const currentBucket = this.data[address]
+      if (currentBucket) {
+        for(let i = 0; i < currentBucket.length; i++){
+          if(currentBucket[i][0] === key) {
+            return currentBucket[i][1]
+          }
+        }
+      }
+      return undefined;
+    }
+  
+    keys() {
+        if (!this.data.length) return undefined;
+     
+        const result = [];
+        for (let i = 0; i < this.data.length; i++) {
+          if (this.data[i] && this.data[i].length) {
+            console.log("item", this?.data[i])
+            if (this.data[i].length > 1) {
+              for (let j = 0; j < this.data[i].length; j++) {
+                result.push(this.data[i][j][0]);
+              }
+            } else { 
+              result.push(this.data[i][0][0]);
+            }
+          }
+        }
+        return result;
+      }
+  }
+  
+  const myHashTable = new HashTable(50);
+  myHashTable.set('grapes', 10000)
+  myHashTable.get('grapes')
+  myHashTable.set('apples', 9)
+  myHashTable.get('apples')
+  myHashTable.set('mangoes', 100)
+  console.log("hash table ", myHashTable)
+  console.log("keys ", myHashTable.keys())
